f2f8a7cef7593d43bfbb63d1e664bfec2ac09994
Ritwik Srivastava <gokku47@Ritwiks-MacBook-Air.local>
2026-05-17 23:57:59 +0530

    feat : added appropriate logging in the user service

--- solids/Projects/Gatekeeper/go.mod
+++ solids/Projects/Gatekeeper/go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/google/uuid v1.6.0
 	github.com/rs/zerolog v1.35.1
 	github.com/spf13/viper v1.21.0
+	golang.org/x/crypto v0.51.0
 	gorm.io/driver/postgres v1.6.0
 	gorm.io/gorm v1.31.1
 )
@@ -53,7 +54,6 @@ require (
 	go.mongodb.org/mongo-driver/v2 v2.6.0 // indirect
 	go.yaml.in/yaml/v3 v3.0.4 // indirect
 	golang.org/x/arch v0.27.0 // indirect
-	golang.org/x/crypto v0.51.0 // indirect
 	golang.org/x/net v0.54.0 // indirect
 	golang.org/x/sync v0.20.0 // indirect
 	golang.org/x/sys v0.44.0 // indirect
--- solids/Projects/Gatekeeper/internal/user/handler.go
+++ solids/Projects/Gatekeeper/internal/user/handler.go
@@ -1,6 +1,7 @@
 package user
 
 import (
+	"gatekeeper/constants"
 	"gatekeeper/constants/errorlogs"
 	"net/http"
 
@@ -21,7 +22,6 @@ func NewUserHandler(service *Service) *UserHandler {
 // Define handler methods for user-related HTTP endpoints here (e.g., CreateUser, GetUser, etc.)
 
 func (h *UserHandler) CreateUser(c *gin.Context) {
-
 	// Bind the incoming JSON request to a struct
 	var req CreateUserRequest
 	if err := c.ShouldBindJSON(&req); err != nil {
@@ -31,15 +31,19 @@ func (h *UserHandler) CreateUser(c *gin.Context) {
 		})
 		return
 	}
+
 	// call the service method to create a user
 	resp, err := h.service.CreateUser(req)
 	if err != nil {
 		switch err {
 		case ErrFirstNameRequired, ErrLastNameRequired, ErrEmailRequired, ErrInvalidEmailFormat,
-			ErrUsernameRequired, ErrPasswordRequired, ErrPasswordTooShort, ErrPasswordInvalidCharacters:
+			ErrUsernameRequired, ErrPasswordRequired, ErrPasswordTooShort,
+			ErrPasswordInvalidCharacters:
+			log.Ctx(c).Warn().Err(err).Msg(constants.LogValidationError)
 			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"message": err.Error()})
 			return
-		case ErrEmailAlreadyExists, ErrUserEmailAlreadyExists, ErrUserUsernameAlreadyExists:
+		case ErrEmailAlreadyExists, ErrUserEmailAlreadyExists:
+			log.Ctx(c).Warn().Err(err).Msg(constants.LogConflictError)
 			c.AbortWithStatusJSON(http.StatusConflict, gin.H{"message": err.Error()})
 			return
 		default:
